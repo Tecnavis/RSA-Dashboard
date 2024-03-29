9b50eb242c83358c727720085ff3a0ff1fd0505a
sajna <sajnaaizanaufal@gmail.com>
2024-03-29 10:10:59 +0530

    new edits

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -2,13 +2,10 @@
 import firebase from "firebase/compat/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// config.ts
-// config.ts
-
 const firebaseConfig = {
-
   apiKey: "AIzaSyAYfaaoi24oJl8dJLTqigiobeRhCpDJ8Oc",
   authDomain: "rsa-dashboard-34773.firebaseapp.com",
   projectId: "rsa-dashboard-34773",
@@ -17,4 +14,11 @@ const firebaseConfig = {
   appId: "1:751667160757:web:6eac73e5039f3249ff00fc"
 };
 
-export default  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+// Get the authentication instance
+const auth = getAuth(app);
+
+export { auth };
+export default app;
